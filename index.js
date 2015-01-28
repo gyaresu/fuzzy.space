3c832cf2feadeabc8748f6d6ab744a44d8d542fb
gyaresu <me@gareth.com.au>
2015-01-28 19:12:52 +0200

    hello world

---
+++ index.js
@@ -0,0 +1,29 @@
+var Hapi = require('hapi');
+
+var server = new Hapi.Server();
+server.connection({ port: 3000 });
+
+server.start(function () {
+  console.log('Server running at: ', server.info.uri);
+});
+
+
+var getConfig = {
+  handler: function (request, reply) {
+             reply("hello");
+           },
+}
+
+var testConfig = {
+  handler: function (request, reply) {
+             reply("test");
+           },
+}
+
+var routes = [
+  { path: '/',      method: 'GET', config: getConfig},
+  { path: '/test',  method: 'GET', config: testConfig}
+]
+
+server.route(routes);
+server.start();
